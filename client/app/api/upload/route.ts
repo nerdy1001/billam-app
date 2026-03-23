2851fe7b52cda0c602548d0b394afcc945a78a57
nerdy1001 <bryanakere89@gmail.com>
2026-03-23 02:44:20 +0100

    11: More fixes to api route handler params

--- client/app/api/upload/route.ts
+++ client/app/api/upload/route.ts
@@ -8,7 +8,7 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_SECRET_KEY,
 });
 
-export async function POST(req: NextRequest, _res: NextResponse) {
+export async function POST(req: NextRequest) {
   try {
     const formData = await req.formData();
     const file = formData.get("file") as File | null;
